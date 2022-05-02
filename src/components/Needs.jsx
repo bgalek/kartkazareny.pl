7834a6d88db33e1a493515aaacd4d998454df383
Bartosz Gałek <bartosz@galek.com.pl>
2022-05-02 21:28:30 +0200

    3 in column

--- src/components/Needs.jsx
+++ src/components/Needs.jsx
@@ -6,11 +6,11 @@ export default function Needs({ items, category }) {
     return (
         <Grid>
             {filterItems(items, category).map((item, i) => (
-                <Grid.Col key={item.name + i} md={6} lg={3}>
-                    <MediaQuery largerThan="sm" styles={{ minHeight: 165 }}>
+                <Grid.Col key={item.name + i} md={6} lg={4}>
+                    <MediaQuery largerThan="sm" styles={{ minHeight: 100 }}>
                         <Card shadow="sm" p="md" sx={{ minHeight: 'auto' }}>
                             <CardBadge item={item}/>
-                            <Text weight={300}>{item.name}</Text>
+                            <Text size="sm" weight={300}>{item.name}</Text>
                         </Card>
                     </MediaQuery>
                 </Grid.Col>
