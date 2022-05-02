e8bce17db4a7c6dd55b445093e22b1b60d235fb6
Bartosz Gałek <bartosz@galek.com.pl>
2022-05-02 22:07:00 +0200

    footer dark mode enabled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,34 @@
-import { Container, Footer, Text } from '@mantine/core';
+import { Container, Footer, Paper, Text } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
 import github from './github.svg';
 
 export function AppFooter() {
+    const preferredColorScheme = useColorScheme();
     return (
         <Footer>
-            <Container size="lg" sx={(theme) => ({
-                padding: theme.spacing.md,
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            })}>
-                <Text>
-                    Contribute on GitHub
-                </Text>
-                <a href="https://github.com/bgalek/kartkazareny.pl">
-                    <img style={{ height: 48, width: 48 }} src={github} alt="github"/>
-                </a>
-            </Container>
+            <Paper>
+                <Container size="lg" sx={(theme) => ({
+                    padding: theme.spacing.md,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                })}>
+                    <Text>
+                        Contribute on GitHub
+                    </Text>
+                    <a href="https://github.com/bgalek/kartkazareny.pl">
+                        <img
+                            style={{
+                                height: 48,
+                                width: 48,
+                                filter: preferredColorScheme === 'dark' ? 'invert(1)' : 'invert(0)'
+                            }}
+                            src={github}
+                            alt="github"
+                        />
+                    </a>
+                </Container>
+            </Paper>
         </Footer>
     )
 }
